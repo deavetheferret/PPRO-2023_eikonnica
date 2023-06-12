05b7cc209d5507ad980025bd0de758a537db63a0
François Devantay <francois.devantay@studentfr.ch>
2023-06-12 17:20:35 +0200

    JS > clean up the file

--- src/js/main.js
+++ src/js/main.js
@@ -6,91 +6,6 @@ const cubeFaceFront = document.querySelector(".cube-face--front");
 const cubeCanva = document.querySelector(".cube-container");
 var interactables = document.querySelectorAll(".interactable");
 
-// const start = document.querySelector(".start-top-left");
-// const end = document.querySelector(".end-top-left");
-// const path = document.querySelector(".path-top-left");
-
-// function updatePath() {
-//   const startX = start.offsetLeft + start.offsetWidth / 2;
-//   const startY = start.offsetTop + start.offsetHeight / 2;
-//   const endX = end.offsetLeft + end.offsetWidth / 2;
-//   const endY = end.offsetTop + end.offsetHeight / 2;
-
-//   const d = `M${startX},${startY} L${endX},${endY}`;
-//   pathTopLeft.setAttribute("d", d);
-// }
-
-// // Get the DOM elements
-// const startTopLeft = document.querySelector(".start-top-left");
-// const startBottomLeft = document.querySelector(".start-bottom-left");
-// const startTopRight = document.querySelector(".start-top-right");
-// const startBottomRight = document.querySelector(".start-bottom-right");
-
-// const endTopLeft = document.querySelector(".end-top-left");
-// const endBottomLeft = document.querySelector(".end-bottom-left");
-// const endTopRight = document.querySelector(".end-top-right");
-// const endBottomRight = document.querySelector(".end-bottom-right");
-
-// const pathTopLeft = document.querySelector(".path-top-left");
-// const pathBottomLeft = document.querySelector(".path-bottom-left");
-// const pathTopRight = document.querySelector(".path-top-right");
-// const pathBottomRight = document.querySelector(".path-bottom-right");
-// // Add similar variables for other paths
-
-// // Function to update the path
-// function updatePath() {
-//   const startTopLeftRect = startTopLeft.getBoundingClientRect();
-//   const startBottomLeftRect = startBottomLeft.getBoundingClientRect();
-//   const startTopRightRect = startTopRight.getBoundingClientRect();
-//   const startBottomRightRect = startBottomRight.getBoundingClientRect();
-
-//   const endTopLeftRect = endTopLeft.getBoundingClientRect();
-//   const endBottomLeftRect = endBottomLeft.getBoundingClientRect();
-//   const endTopRightRect = endTopRight.getBoundingClientRect();
-//   const endBottomRightRect = endBottomRight.getBoundingClientRect();
-
-//   // Update the path coordinates
-//   pathTopLeft.setAttribute(
-//     "da",
-//     `M ${startTopLeftRect.right},${startTopLeftRect.bottom} L ${endTopLeftRect.left},${endTopLeftRect.top}`
-//   );
-//   pathBottomLeft.setAttribute(
-//     "db",
-//     `M ${startBottomLeftRect.right},${startBottomLeftRect.top} L ${endBottomLeftRect.left},${endBottomLeftRect.bottom}`
-//   );
-//   pathTopRight.setAttribute(
-//     "dc",
-//     `M ${startTopRightRect.left},${startTopRightRect.bottom} L ${endTopRightRect.right},${endTopRightRect.top}`
-//   );
-//   pathBottomRight.setAttribute(
-//     "dd",
-//     `M ${startBottomRightRect.left},${startBottomRightRect.top} L ${endBottomRightRect.right},${endBottomRightRect.bottom}`
-//   );
-// }
-
-// // Add event listeners for any window resize or relevant changes
-// window.addEventListener("resize", updatePath);
-// // Add similar event listeners for any other relevant changes
-
-// document.addEventListener("mousemove", function (e) {
-//   const x = e.clientX;
-//   const y = e.clientY;
-
-//   cursor.style.left = x + "px";
-//   cursor.style.top = y + "px";
-
-//   cubeCanva.addEventListener("mousemove", function () {
-//     cubeFaceFront.style.top = y / 2 + "px";
-//     cubeFaceFront.style.left = x / 2 + "px";
-
-//     cubeFaceBack.style.left = x * 2 + "px";
-//     cubeFaceBack.style.top = y * 2 + "px";
-//   });
-//   updatePath();
-
-//   console.log(x, y);
-// });
-
 const startTopLeft = document.querySelector(".start-top-left");
 const startBottomLeft = document.querySelector(".start-bottom-left");
 const startTopRight = document.querySelector(".start-top-right");
@@ -106,7 +21,6 @@ const pathBottomLeft = document.querySelector(".path-bottom-left");
 const pathTopRight = document.querySelector(".path-top-right");
 const pathBottomRight = document.querySelector(".path-bottom-right");
 
-// Function to update the path
 function updatePath() {
   const startTopLeftRect = startTopLeft.getBoundingClientRect();
   const startBottomLeftRect = startBottomLeft.getBoundingClientRect();
@@ -118,7 +32,6 @@ function updatePath() {
   const endTopRightRect = endTopRight.getBoundingClientRect();
   const endBottomRightRect = endBottomRight.getBoundingClientRect();
 
-  // Update the path coordinates
   pathTopLeft.setAttribute(
     "d",
     `M ${startTopLeftRect.right},${startTopLeftRect.bottom} L ${endTopLeftRect.left},${endTopLeftRect.top}`
@@ -137,9 +50,7 @@ function updatePath() {
   );
 }
 
-// Add event listeners for any window resize or relevant changes
 window.addEventListener("resize", updatePath);
-// Add similar event listeners for any other relevant changes
 
 document.addEventListener("mousemove", function (e) {
   const x = e.clientX;
